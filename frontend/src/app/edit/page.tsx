037b1c2b06609983e9fd1a9779b33b4597a8ac5a
yoshi0319 <yoshi3YH@icloud.com>
2025-02-19 21:34:08 +0900

    スコアとtipsを配置しコンポーネントに分割

--- frontend/src/app/edit/page.tsx
+++ frontend/src/app/edit/page.tsx
@@ -1,11 +1,15 @@
 import EditPartsMenu from '../components/edit/EditPartsMenu'
 import NextButton from '../components/edit/NextButton'
+import ScorePanel from '../components/edit/ScorePanel'
+import TipsPanel from '../components/edit/TipsPanel'
 import styles from './styles.module.css'
 
 export default function EditPage() {
     return (
         <div className={styles.container}>
             <EditPartsMenu />
+            <ScorePanel />
+            <TipsPanel />
             <div className={styles.buttonGroup}>
                 <NextButton type="retire" position="left" text="リタイア" />
                 <NextButton type="rotate" position="center" text="回転" />
