2f51a5adaccebdab48da1890b497b47c50f6fb87
yoshi0319 <yoshi3YH@icloud.com>
2025-02-21 15:29:08 +0900

    パーツ一覧の矢印忘れてたので追加

--- frontend/src/app/edit/page.tsx
+++ frontend/src/app/edit/page.tsx
@@ -11,12 +11,11 @@ import GameGrid from '../components/edit/GameGrid'
 import { usePartsStore } from '@/app/store/partsStore'
 
 export default function EditPage() {
-    const [showPartsList, setShowPartsList] = useState(false)
     const { rotatePart, placePart } = usePartsStore();
 
     return (
         <div className={styles.container}>
-            <EditPartsMenu onPartsListClick={() => setShowPartsList(true)} />
+            <EditPartsMenu />
             <GameGrid />
             <ScorePanel />
             <TipsPanel />
@@ -25,7 +24,6 @@ export default function EditPage() {
                 <NextButton type="rotate" position="center" text="回転" onClick={rotatePart} />
                 <NextButton type="place" position="right" text="設置" onClick={placePart} />
             </div>
-            {showPartsList && <PartsList onClose={() => setShowPartsList(false)} />}
         </div>
     )
 }
