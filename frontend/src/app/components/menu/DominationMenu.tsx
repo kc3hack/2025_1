4eb8e2f8c869242dc48a9040552d892b4e9ed420
uniuni7 <uniuni0141@docomo.ne.jp>
2025-02-23 05:57:34 +0900

    統治度100%で色がつくようにしました

--- frontend/src/app/components/menu/DominationMenu.tsx
+++ frontend/src/app/components/menu/DominationMenu.tsx
@@ -1,6 +1,12 @@
 import styles from './DominationMenu.module.css';
 
-const DominationMenu = () => {
+interface DominationMenuProps {
+  dominationLevels: { [region: string]: number };
+}
+
+const DominationMenu = ({ dominationLevels = {} }: DominationMenuProps) => {
+  const totalDomination = Object.values(dominationLevels).reduce((acc, level) => acc + level, 0) / (Object.keys(dominationLevels).length || 1);
+
   return (
     <div className={styles.governanceFrame}>
       <div className={styles.menuFrameBackground} />
@@ -9,7 +15,7 @@ const DominationMenu = () => {
           <tbody>
             <tr>
               <td>日本の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>24%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{totalDomination.toFixed(0)}%</td>
             </tr>
             <tr>
               <td colSpan={2} style={{ padding: '0px 0' }}>
@@ -18,31 +24,31 @@ const DominationMenu = () => {
             </tr>
             <tr>
               <td>関西の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>50%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{dominationLevels.Kansai || 0}%</td>
             </tr>
             <tr>
               <td>中部の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>0%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{dominationLevels.Chubu || 0}%</td>
             </tr>
             <tr>
               <td>関東の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>0%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{dominationLevels.Kanto || 0}%</td>
             </tr>
             <tr>
               <td>東北の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>0%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{dominationLevels.Tohoku || 0}%</td>
             </tr>
             <tr>
               <td>中国の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>0%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{dominationLevels.Chugoku || 0}%</td>
             </tr>
             <tr>
               <td>四国の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>0%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{dominationLevels.Shikoku || 0}%</td>
             </tr>
             <tr>
               <td>九州の統治度</td>
-              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>0%</td>
+              <td style={{ textAlign: 'right', paddingLeft: '20px' }}>{dominationLevels.Kyushu || 0}%</td>
             </tr>
           </tbody>
         </table>
@@ -51,4 +57,4 @@ const DominationMenu = () => {
   );
 };
 
-export default DominationMenu; 
+export default DominationMenu;
